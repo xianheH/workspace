cb292f404f30490e4ade657f45db0508982853b4
xianheH <XMH_xianhe_huang@hotmail.com>
2014-04-18 17:16:55 -0400

    Angry Teemo
    first iteration of the game

--- game/README.txt.txt
+++ game/README.txt.txt
@@ -1,2 +1,9 @@
 This is my first web app game made using Javascript CSS and HTML. 
-The CSS is taken from a snake game tutorial and will be updated as the game progresses.
+
+
+*********DISCLAMER****************
+
+This game is my sample test. I just want to see what it is like coding a javascript game. 
+Less than 12 hours was spend on this game.
+Alot of variable is hard coded and has yet to be generalized. Alot of shortcuts were used
+and is not acceptable for launch. This in no way reflect on my coding style or my coding skills.
--- game/angry-teemo/game.js
+++ game/angry-teemo/game.js
@@ -7,7 +7,15 @@ var gameBoard = new GameBoard;
 var gameEvent;
 var increaseScore;
 var pipeNumber = 0;
-var pipeArray = new Array(); 
+var pipeArray = new Array();
+var birdHeight;
+var MOVEDOWN = 7;
+var MOVEUP = 30;
+var SCORE = 5;
+var PIPEMOVE = 5;
+var WIDTH = 78;
+var LEFT = 322;
+
 
 $(document).ready(function() {
     $('body').keydown(keyPresseHandler);
@@ -36,20 +44,20 @@ function startGame() {
 		bird.moveDown();
 		gameBoard.updateScore();
 		gameBoard.pipeMove();
-	}, 1000);
+	}, 100);
 }
 
 function spawn() { 
 	var pipeName ='pipe'+pipeNumber;
-	gameBoard.createPipe(pipeName, pipeName, "pipe");
+	gameBoard.createPipe(pipeName, pipeName, "pipe2");
 	var topPipeHeight = Math.floor((Math.random()*300)+1);
-	gameBoard.pipeSetting(pipeName, 400-topPipeHeight, topPipeHeight, 322, 78);
+	gameBoard.pipeSetting(pipeName, 400-topPipeHeight, topPipeHeight, LEFT, WIDTH);
 	pipeArray[pipeArray.length] = pipeNumber;
 	pipeNumber++;
 	pipeName ='pipe'+pipeNumber;
 	gameBoard.createPipe(pipeName, pipeName, "pipe1");
-	var bottomPipeHeight = 320 - topPipeHeight;
-	gameBoard.pipeSetting(pipeName, 0, bottomPipeHeight, 322, 78);
+	var bottomPipeHeight = 250 - topPipeHeight;
+	gameBoard.pipeSetting(pipeName, 0, bottomPipeHeight, LEFT, WIDTH);
 	pipeArray[pipeArray.length] = pipeNumber;
 	pipeNumber++;
 }
@@ -75,17 +83,32 @@ function GameBoard() {
 		$('#'+idname).css('left', left+'px');
 	};
 
+	this.detectCollision = function (src, idname) {
+		if (src.indexOf("pipe1") !== -1 && birdHeight < parseInt($('#'+idname).css('height'))){
+			gameBoard.clearBoard();
+			gameBoard.showLoseMessage();
+		}
+		else if (src.indexOf("pipe2") !== -1 && birdHeight > parseInt($('#'+idname).css('top'))){
+			gameBoard.clearBoard();
+			gameBoard.showLoseMessage();
+		}  
+	};
+
 	this.pipeMove = function () {
 		var canSpawn = true;
 		var idname, distanceLeft;
 		for (var i = 0; i < pipeArray.length; i++) {
 			idname ='pipe' + pipeArray[i];
-			distanceLeft = parseInt($('#'+idname).css('left')) - 30;
+			distanceLeft = parseInt($('#'+idname).css('left')) - PIPEMOVE;
 			$('#'+idname).css('left', distanceLeft +'px');
-			if(distanceLeft < 0) {
+			if (distanceLeft < 40) {
+				this.detectCollision(document.getElementById(idname).src, idname);
+			}
+			if(distanceLeft < -2*WIDTH) {
 				var pipeClass ='div.pipe'+pipeArray[i];
 			    $(pipeClass).remove();
-			    delete pipeArray[i];
+			    pipeArray.splice(i,1)
+			    i--;
 			    if (canSpawn) {
 			    	spawn();
 			    }
@@ -108,7 +131,7 @@ function GameBoard() {
 	};
 	
 	this.clearGameInfo = function() {
-		height = 60;
+		birdHeight = 60;
 		$('#score').html('0');
 		$('#gamemsg').css('visibility','hidden');
 		$('#speed').html('1');
@@ -116,7 +139,7 @@ function GameBoard() {
 		
 	this.updateScore = function() {
 		var $currentScore = Number($('#score').html());
-		$currentScore+=10;
+		$currentScore+=SCORE;
 		$('#score').html($currentScore);
 	};
 	
@@ -142,22 +165,22 @@ function Bird() {
 	$bird.prepend('<img id="bird" src="teemo.jpg" />');
 	$('#gameField').append($bird);
 	$('#bird').css('position', 'absolute');
-	$('#bird').css('top', height+'px');
+	$('#bird').css('top', birdHeight+'px');
 
 	this.moveUp = function(){
-		height-=10;
-		if (height < 0) {
-			height = 0;
+		birdHeight-=MOVEUP;
+		if (birdHeight < 0) {
+			birdHeight = 0;
 		}
-		$('#bird').css('top', height+'px');
+		$('#bird').css('top', birdHeight+'px');
 	};
 
 	this.moveDown = function(){
-		height+=20;
-		if (height > 290) {
+		birdHeight+=MOVEDOWN;
+		if (birdHeight > 350) {
 			gameBoard.clearBoard();
 			gameBoard.showLoseMessage();
 		}
-		$('#bird').css('top', height+'px');
+		$('#bird').css('top', birdHeight+'px');
 	}
 };
